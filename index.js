a9dcb47875a08dbb4f0a5294d865c771a3174106
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-03-10 10:59:00 -0700

    configured port

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require("express");
+require("dotenv").config();
 
+const PORT = process.env.PORT || 9000;
+
+const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
   res.end("<h1>heelo world<h1>");
 });
 
-server.listen(9000, () => {
-  console.log("server is runnning on port 9000!");
+server.listen(PORT, () => {
+  console.log(process.env.MY_ENV);
+  console.log(
+    `server is runnning on port ${PORT}, user is ${process.env.USER}!`
+  );
 });
